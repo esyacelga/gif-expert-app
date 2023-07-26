6d5bb7b216111132bbf9bcb485f265f0cd4cc4bb
esyacelga <esyacelga@hotmail.com>
2023-07-25 22:55:16 -0500

    Agregado de objeto a lista

--- src/commponents/GifGrid.jsx
+++ src/commponents/GifGrid.jsx
@@ -1,13 +1,18 @@
+const getFifs = async (category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=Od8yHVz27YJizJHMkoCfptKKBqA3sWdj&q=${category}&limit=20`
+    const resp = await fetch(url)
+    const {data} = await resp.json();
+    const gifs = data.map(img => ({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url
+    }))
+    console.log(gifs);
+    return gifs;
+}
+
 export const GifGrid = ({category}) => {
-    const getFifs = async () => {
-        const ulr = `https://api.giphy.com/v1/gifs/search?api_key=Od8yHVz27YJizJHMkoCfptKKBqA3sWdj&q=${category}&limit=20`
-        const resp = await fetch(url)
-        const {data} = await resp.json();
-        const gifs = data.map(im)
-    }
-
-    getFifs();
-
+    getFifs(category);
     return (
         <><h3>{category}</h3>
             <p>Hola mundo</p>
